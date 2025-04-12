f879d79d1a35053172fc52b857d9dce5820162e0
Nahidbd270 <Nahidbd270@gmail.com>
2025-04-12 13:02:53 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -194,13 +194,13 @@ async def start_command(client: Client, message: Message):
                     link = await get_shortlink(SHORTLINK_API_URL, SHORTLINK_API_KEY,f'{newLink}')
                     if USE_PAYMENT:
                         btn = [
-                        [InlineKeyboardButton("Click Here 👆", url=link),
+                        [InlineKeyboardButton("Download Link 👆", url=link),
                         InlineKeyboardButton('How to open this link 👆', url=TUT_VID)],
                         [InlineKeyboardButton("Buy Premium plan", callback_data="buy_prem")]
                         ]
                     else:
                         btn = [
-                        [InlineKeyboardButton("Click Here 👆", url=link)],
+                        [InlineKeyboardButton("Download Link 👆", url=link)],
                         [InlineKeyboardButton('How to open this link 👆', url=TUT_VID)]
                         ]
                     await message.reply(f"Total clicks {clicks}. Here is your link 👇.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
@@ -246,13 +246,13 @@ async def start_command(client: Client, message: Message):
             link = await get_shortlink(SHORTLINK_API_URL, SHORTLINK_API_KEY,f'https://telegram.dog/{client.username}?start=verify_{token}')
             if USE_PAYMENT:
                 btn = [
-                [InlineKeyboardButton("Click Here 👆", url=link),
+                [InlineKeyboardButton("Download link 👆", url=link),
                 InlineKeyboardButton('How to open this link 👆', url=TUT_VID)],
                 [InlineKeyboardButton("Buy Premium plan", callback_data="buy_prem")]
                 ]
             else:
                 btn = [
-                [InlineKeyboardButton("Click Here 👆", url=link)],
+                [InlineKeyboardButton("Download link 👆", url=link)],
                 [InlineKeyboardButton('How to open this link 👆', url=TUT_VID)]
                 ]
             await message.reply(f"Your Ads token is expired, refresh your token and try again. \n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for {get_exp_time(VERIFY_EXPIRE)} after passing the ad", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
